fbf13e8d24a613633a6c01acaf7476a7bd4481f8
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-11-28 18:58:14 +0530

    Solved 4 questions from the video

--- Lecture 7/1.py
+++ Lecture 7/1.py
@@ -4,3 +4,7 @@
 # we are learning File I/O
 # using Java.
 # I like programming in Java.
+
+with open("practice.txt", "w") as f:
+    f.write("Hi everyone\nwe are learning File I/O\n")
+    f.write("using Java.\nI like programming in Java.")
--- Lecture 7/2.py
+++ Lecture 7/2.py
@@ -1,1 +1,11 @@
 # WAF that replace all occurrences of “java” with “python” in above file.
+
+with open("practice.txt", "r") as f:
+    data = f.read()
+
+new_data = data.replace("Java", "Python")
+print(new_data)
+
+with open("practice.txt", "w") as f:
+    f.write(new_data)
+
