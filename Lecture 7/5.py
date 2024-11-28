32702898422cafa40ed42fc6fa6c38e01e94d1d5
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-11-28 19:05:34 +0530

    Solved 5th question

--- Lecture 7/5.py
+++ Lecture 7/5.py
@@ -1,1 +1,21 @@
 # From a file containing numbers separated by comma, print the count of even numbers.
+
+count = 0
+with open("practice.txt", "r") as f:
+    data = f.read()
+
+    nums = data.split(",")
+    for val in nums: 
+        if(int(val) % 2 == 0):
+            count += 1
+
+print(count)
+
+    # print(nums)
+    # num = ""
+    # for i in range(len(data)):
+    #     if(data[i] == ","):
+    #         print(int(num))
+    #         num = ""
+    #     else:
+    #         num += data[i]
